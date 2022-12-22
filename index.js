7c0b23c609771511c27a08c72917a741d73d5db1
CodeNeBo <nechitaylob@gmail.com>
2022-12-22 15:53:42 +0200

    added phone formatter

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
 function openDiv() {
     document.getElementById("messageWrap").style.display = "flex";
-}
+};
 
 function closeDiv() {
     document.getElementById("messageWrap").style.display = "none";
-}
+};
+
+function placeholderNum() {
+    document.getElementById("pNum").placeholder="(xxx)-xxx-xx-xx";
+};
+
+function formatPhoneNumber(value) {
+    if (!value) return value;
+    const phoneNumber = value.replace(/[^\d]/g, "");
+    const phoneNumberLength = phoneNumber.length;
+    if (phoneNumberLength < 4) return phoneNumber;
+    if (phoneNumberLength < 7) {
+        return `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3)}`;
+    }
+    return `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3,6,)}-${phoneNumber.slice(6,8)}-${phoneNumber.slice(8,10)}`;
+};
+
+function phoneFormat() {
+    const inputField = document.getElementById("pNum");
+    const formattedInputValue = formatPhoneNumber(inputField.value);
+    inputField.value = formattedInputValue
+};
