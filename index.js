53bce8ca0aa6b566f00a1b715ac2a402cce8aaba
CodeNeBo <nechitaylob@gmail.com>
2022-12-22 16:12:25 +0200

    removed format number

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
 function placeholderNum() {
     document.getElementById("pNum").placeholder="(xxx)-xxx-xx-xx";
 };
-
-function formatPhoneNumber(value) {
-    if (!value) return value;
-    const phoneNumber = value.replace(/[^\d]/g, "");
-    const phoneNumberLength = phoneNumber.length;
-    if (phoneNumberLength < 4) return phoneNumber;
-    if (phoneNumberLength < 7) {
-        return `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3)}`;
-    }
-    return `(${phoneNumber.slice(0,3)}) ${phoneNumber.slice(3,6,)}-${phoneNumber.slice(6,8)}-${phoneNumber.slice(8,10)}`;
-};
-
-function phoneFormat() {
-    const inputField = document.getElementById("pNum");
-    const formattedInputValue = formatPhoneNumber(inputField.value);
-    inputField.value = formattedInputValue
-};
